a1911494b98bc1cd6cb9a8be784576c653db60c7
hugosenari <hugosenari@gmail.com>
2023-12-07 16:39:15 -0300

    feat: flake it till you make it

---
+++ modules.nix
@@ -0,0 +1,44 @@
+{ pkgs, lib, config, ...}:
+let
+  toBG = { red, green, blue }:
+    "${toString(255.0 / red)}, "
+    "${toString(255.0 / green)}, "
+    "${toString(255.0 / blue)})";
+in
+{
+  options.nixos-boot.enable = lib.mkEnableOption "nixos-boot";
+  options.nixos-boot.bgColor.red = lib.mkOption {
+    type    = lib.types.ints.between 0 255;
+    default = 255;
+  };
+  options.nixos-boot.bgColor.green = lib.mkOption {
+    type    = lib.types.ints.between 0 255;
+    default = 255;
+  };
+  options.nixos-boot.bgColor.blue = lib.mkOption {
+    type    = lib.types.ints.between 0 255;
+    default = 255;
+  };
+  options.nixos-boot.theme = lib.mkOption {
+    type    = lib.types.enum [ "load_unload" ];
+    default = "load_unload";
+  };
+  options.nixos-boot.duration = lib.mkOption {
+    type    = lib.types.float;
+    default = 0.0;
+  };
+  config.nixpkgs.overlays = [(self: super: {
+    nixos-boot = super.callPackage ./default.nix {
+      theme   = config.nixos-boot.theme;
+      bgColor = toBG config.nixos-boot.bgColor;
+    };
+  })];
+  config.boot.plymouth = lib.mkIf config.nixos-boot.enable {
+    enable = true;
+    themePackages = [ pkgs.nixos-boot ];
+    theme = config.nixos-boot.theme;
+  };
+  config.systemd.services.plymouth-quit = lib.mkIf (config.nixos-boot.enable && config.nixos-boot.duration > 0.0) {
+    preStart = "${pkgs.coreutils}/bin/sleep ${toString config.nixos-boot.duration}";
+  };
+}
